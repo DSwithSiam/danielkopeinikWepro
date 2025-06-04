dd0179e072420834bb4b2e034ffcab42cace8828
DSwithSiam <masipulislam@gmail.com>
2025-06-04 16:29:15 +0600

    Add ChatBot model fields and serializers; implement create and chat message views

--- chat_bot/models.py
+++ chat_bot/models.py
@@ -5,9 +5,21 @@ from accounts.models import CustomUser  # Assuming you have a User model in acco
 class ChatBot(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='chat_bots')
     name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True, null=True)
+    style = models.CharField(max_length=50, choices=[
+        ('modern', 'Modern'),
+        ('classic', 'Classic'),
+        ('minimal', 'Minimal'),
+    ])
+
+    image = models.ImageField(upload_to='chat_bots_images/', blank=True, null=True)
+    csv_file = models.FileField(upload_to='chat_bots_csv/', blank=True, null=True)
+    website_link = models.URLField(blank=True, null=True)
+    unique_id = models.UUIDField(default=models.UUIDField().default, editable=False, unique=True)
+    count_messages = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+
